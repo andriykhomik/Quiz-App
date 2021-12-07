3f0a4587eb14f39a8217cddba683be2c3e4750fe
andriykhomik <andriykhomik1992@gmail.com>
2021-12-07 14:41:13 +0200

    Finished this project by tutorial

--- main.js
+++ main.js
@@ -35,54 +35,67 @@ const quizData = [
 
 
 
-const submit = document.querySelector('.submit');
-const labelA = document.querySelector('#a_text');
-const labelB = document.querySelector('#b_text');
-const labelC = document.querySelector('#c_text');
-const labelD = document.querySelector('#d_text');
-const question = document.querySelector('#question');
-const quizHeader = document.querySelector('.quiz-header');
+const quiz = document.querySelector('#quiz');
+const answerEls = document.querySelectorAll('.answer');
+const questionEl = document.querySelector('#question');
+const a_text = document.querySelector('#a_text');
+const b_text = document.querySelector('#b_text');
+const c_text = document.querySelector('#c_text');
+const d_text = document.querySelector('#d_text');
+const submitBtn = document.querySelector('#submit');
 
 let currentQuiz = 0;
-let correctCounter = 0;
+let score = 0;
 
-render();
+loadQuiz();
 
-submit.addEventListener('click', ()=> {
+function loadQuiz(){
+    deselectAnswers();
 
-    const inputs = document.querySelectorAll('li input');
+    const currentQuizData = quizData[currentQuiz];
 
-    inputs.forEach(input => {
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
 
-        if (input.checked && input.id === quizData[currentQuiz].correct){
-            correctCounter++;
+function deselectAnswers(){
+    answerEls.forEach(answerEl => {
+        answerEl.checked = false;
+    })
+}
+
+function getSelected(){
+    let answer;
+
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked){
+            answer = answerEl.id;
+        }
+    })
+    return answer;
+}
+
+submitBtn.addEventListener('click', ()=>{
+    const answer = getSelected();
+
+    if (answer){
+        if (answer === quizData[currentQuiz].correct){
+            score++;
         }
 
-        input.checked = false;
-    })
+        currentQuiz++;
 
-    if (submit.innerText === 'Reload'){
-        location.reload();
+        if (currentQuiz < quizData.length){
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+                <h2>You answered correctly at ${score}/${quizData.length} questions</h2>
+                
+                <button class="submit" onclick="location.reload() ">Reload</button>
+            `
+        }
     }
-
-    currentQuiz++;
-    render();
-
 })
-
-function render(){
-
-    if (currentQuiz > quizData.length - 1){
-
-        quizHeader.innerHTML = `<h2>You answered ${correctCounter}/${quizData.length} questions correctly<h2/>`
-        submit.innerText = `Reload`;
-
-    } else {
-
-        question.innerText = quizData[currentQuiz].question
-        labelA.innerText = quizData[currentQuiz].a;
-        labelB.innerText = quizData[currentQuiz].b;
-        labelC.innerText = quizData[currentQuiz].c;
-        labelD.innerText = quizData[currentQuiz].d;
-    }
-}
